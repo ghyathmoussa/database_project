9f2ce21963ebe868e1f70aff2942ce167d283883
Ghyath Moussa <ghyathmoussa@hotmail.com>
2022-01-02 13:01:11 +0300

    add axios and fetch data function \n remove unnecessary fields from d…

--- client/src/components/Forms/UpdateStationForm.js
+++ client/src/components/Forms/UpdateStationForm.js
@@ -1,40 +1,64 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form, Row, Button, Col} from "react-bootstrap";
+import { Form, Row, Button, Col } from "react-bootstrap";
+import axios from 'axios'
 
-const UpdateStationForm = (props) =>  {
+const UpdateStationForm = (props) => {
+  const [data, setData] = useState({
+    snumber: 0,
+    sname: '',
+    city: '',
+  })
+  function fetchData() {
+    const port = 'http://localhost:4000/api/station/update'
+
+    axios.post(port,data).then(res =>{
+      console.log(res.data)
+
+    })
+      .catch(err => console.log(err.message))
+  }
   return (
-    
-    <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Enter the Station Number of the Station You Want to Update</Form.Label>
-            <Form.Control placeholder="Station Number"/>
-          </Form.Group>
-        </Row>
 
-        <h2>New Information</h2>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        console.log('submitted')
+        fetchData()
+      }}
+    >
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Enter the Station Number of the Station You Want to Update</Form.Label>
+          <Form.Control placeholder="Station Number" name='snumber' value={data.snumber} onChange={(e) => setData({...data,snumber:parseInt(e.target.value)})} />
+        </Form.Group>
+      </Row>
 
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Staion Name</Form.Label>
-            <Form.Control placeholder="Staion Name"/>
-          </Form.Group>
+      <h2>New Information</h2>
 
-          <Form.Group as={Col}>
-            <Form.Label>City</Form.Label>
-            <Form.Control placeholder="City" />
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Staion Number</Form.Label>
-            <Form.Control placeholder="Staion Number" />
-           </Form.Group>
-        </Row>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Staion Name</Form.Label>
+          <Form.Control placeholder="Staion Name" name='sname' value={data.sname} onChange={(e) => setData({...data,sname:e.target.value})} />
+        </Form.Group>
+
+        <Form.Group as={Col}>
+          <Form.Label>City</Form.Label>
+          <Form.Control placeholder="City" name='city' value={data.city} onChange={(e) => setData({...data,city:e.target.value})} />
+        </Form.Group>
+      </Row>
+      {/* 
+        * no need to change ID of Station
+      */}
+      {/* <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Staion Number</Form.Label>
+          <Form.Control placeholder="Staion Number" />
+        </Form.Group>
+      </Row> */}
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
     </Form>
   );
 }
--- client/src/components/Forms/UpdateTaxiForm.js
+++ client/src/components/Forms/UpdateTaxiForm.js
@@ -1,40 +1,65 @@
 
-import * as React from 'react';
+import React,{useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form, Row, Button, Col} from "react-bootstrap";
+import { Form, Row, Button, Col } from "react-bootstrap";
+import axios from 'axios'
 
-const UpdateTaxiForm = (props) =>  {
+const UpdateTaxiForm = (props) => {
+  const [data, setData] = useState({
+    plate: '',
+    model: '',
+    snumber: 0
+  })
+  function fetchData() {
+    const port = 'http://localhost:4000/api/taxi/update'
+
+    axios.post(port,data).then(res =>{
+      console.log(res.data)
+
+    })
+      .catch(err => console.log(err.message))
+  }
   return (
-    
-    <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Enter the Plate Number of the Taxi You Want to Update</Form.Label>
-            <Form.Control placeholder="Plate Number"/>
-          </Form.Group>
-        </Row>
 
-        <h2>New Information</h2>
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Plate Number</Form.Label>
-            <Form.Control placeholder="Plate Number"/>
-          </Form.Group>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        console.log('submitted')
+        fetchData()
+      }}
+    >
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Enter the Plate Number of the Taxi You Want to Update</Form.Label>
+          <Form.Control placeholder="Plate Number" name='plate' value={data.plate} onChange={(e) => setData({...data,plate:e.target.value})} />
+        </Form.Group>
+      </Row>
 
-          <Form.Group as={Col}>
-            <Form.Label>Car Model</Form.Label>
-            <Form.Control placeholder="Car Model" />
-          </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col}>
-            <Form.Label>Station Number</Form.Label>
-            <Form.Control placeholder="Station Number"/>
-          </Form.Group>
-        </Row>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+      <h2>New Information</h2>
+      <Row className="mb-3">
+        {/**
+         * no need to change the plate number
+         */
+         }
+        {/* <Form.Group as={Col}>
+          <Form.Label>Plate Number</Form.Label>
+          <Form.Control placeholder="Plate Number" />
+        </Form.Group> */}
+
+        <Form.Group as={Col}>
+          <Form.Label>Car Model</Form.Label>
+          <Form.Control placeholder="Car Model" name='model' value={data.model} onChange={(e) => setData({...data,model:e.target.model})} />
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col}>
+          <Form.Label>Station Number</Form.Label>
+          <Form.Control placeholder="Station Number" name='snumber' value={data.snumber} onChange={(e) => setData({...data,snumber:parseInt(e.target.value)})} />
+        </Form.Group>
+      </Row>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
     </Form>
   );
 }
